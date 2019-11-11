71e1d11eb8825b3394e333116f6b1df935356bf5
Arne Tesch <arnetesch@gmail.com>
2019-11-11 18:37:57 +0100

    fix

--- mongoose/models/meetup.js
+++ mongoose/models/meetup.js
@@ -1,25 +1,25 @@
-const mongoose = require("mongoose");]
+const mongoose = require("mongoose");
 const Schema = mongoose.schema;
 
 const meetupSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: Date,
-        required: true
-    },
-    location: {
-        type: String,
-        required: true
-    },
-    title: {
-        type: String,
-        required: true
-    },
-})
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  }
+});
